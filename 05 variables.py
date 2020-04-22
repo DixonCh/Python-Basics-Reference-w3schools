ef93bf8c0eac6dc7795deaf5b034ff23bd6f29ed
DixonCh <dixonchaudhary7@gmail.com>
2020-04-22 21:44:57 +0545

    Update 05 variables.py

--- 05 variables.py
+++ 05 variables.py
@@ -1,12 +1,12 @@
 Python Variables
 
+Here we will discuss about how to create the variables.
 Creating Variables
 Variables are containers for storing data values.
 
 Unlike other programming languages, Python has no command for declaring a variable.
 
 A variable is created the moment you first assign a value to it.
-
 
 
 Example
