f2681716e3a51f3d57068c6b624597054b0a1324
DixonCh <dixonchaudhary7@gmail.com>
2020-04-22 21:35:13 +0545

    Update 03 syntax.py

--- 03 syntax.py
+++ 03 syntax.py
@@ -8,11 +8,13 @@ Python Comments
 Exercises
 Execute Python Syntax
 As we learned in the previous page, Python syntax can be executed by writing directly in the Command Line:
+'''
 
 >>> print("Hello, World!")
 Hello, World!
 Or by creating a python file on the server, using the .py file extension, and running it in the Command Line:
 
+  '''
 C:\Users\Your Name>python myfile.py
 Python Indentation
 Indentation refers to the spaces at the beginning of a code line.
@@ -22,6 +24,8 @@ Where in other programming languages the indentation in code is for readability 
 Python uses indentation to indicate a block of code.
 
 Example
+'''
+  
 if 5 > 2:
   print("Five is greater than two!")
 Python will give you an error if you skip the indentation:
