d0dfc3a24836a2370a335b1f83b7e8957b91b6fe
DixonCh <dixonchaudhary7@gmail.com>
2020-04-24 14:27:00 +0545

    Update 13 tuples.py

--- 13 tuples.py
+++ 13 tuples.py
@@ -1,28 +1,33 @@
-Python Tuples
-Tuple
+#Python Tuples
+'''Tuple
 A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
 
 Example
 Create a Tuple:
+'''
 
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+'''
 Access Tuple Items
 You can access tuple items by referring to the index number, inside square brackets:
 
 Example
 Print the second item in the tuple:
-
+'''
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[1])
+'''
 Negative Indexing
 Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last item etc.
 
 Example
 Print the last item of the tuple:
+'''
 
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[-1])
+'''
 Range of Indexes
 You can specify a range of indexes by specifying where to start and where to end the range.
 
@@ -30,9 +35,11 @@ When specifying a range, the return value will be a new tuple with the specified
 
 Example
 Return the third, fourth, and fifth item:
+'''
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:5])
+'''
 Note: The search will start at index 2 (included) and end at index 5 (not included).
 
 Remember that the first item has index 0.
@@ -42,6 +49,7 @@ Specify negative indexes if you want to start the search from the end of the tup
 
 Example
 This example returns the items from index -4 (included) to index -1 (excluded)
+'''
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[-4:-1])
