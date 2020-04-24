94ab4add6d0afbc6fe369ed5f7de81da19fa211d
DixonCh <dixonchaudhary7@gmail.com>
2020-04-24 14:24:14 +0545

    Update 11 operators.py

--- 11 operators.py
+++ 11 operators.py
@@ -12,11 +12,12 @@ Identity operators
 Membership operators
 Bitwise operators
 # ****************************************************
-Python Arithmetic Operators
+Python Arithmetic Operators:
+
 Arithmetic operators are used with numeric values to perform common mathematical operations:
 
 Operator	Name	                Example	     Try it
-
+'''
 +	       Addition	            x + y	
 -	       Subtraction	         x - y	
 *	       Multiplication	      x * y	
@@ -25,10 +26,11 @@ Operator	Name	                Example	     Try it
 **	      Exponentiation	      x ** y	
 //	      Floor division	      x // y	
 
-Python Assignment Operators
-Assignment operators are used to assign values to variables:
+#Python Assignment Operators
+#Assignment operators are used to assign values to variables:
 
-Operator	Example	Same As	Try it
+#Operator	Example	Same As	Try it
+
 =	x = 5	x = 5	
 +=	x += 3	x = x + 3	
 -=	x -= 3	x = x - 3	
@@ -43,18 +45,19 @@ Operator	Example	Same As	Try it
 >>=	x >>= 3	x = x >> 3	
 <<=	x <<= 3	x = x << 3	
 
-Python Comparison Operators
-Comparison operators are used to compare two values:
+#Python Comparison Operators
+#Comparison operators are used to compare two values:
 
-Operator	Name	Example	Try it
+#Operator	Name	Example	Try it
+
 ==	Equal	x == y	
 !=	Not equal	x != y	
 >	Greater than	x > y	
 <	Less than	x < y	
 >=	Greater than or equal to	x >= y	
 <=	Less than or equal to	x <= y	
-Python Logical Operators
-Logical operators are used to combine conditional statements:
+#Python Logical Operators
+#Logical operators are used to combine conditional statements:
 
 Operator	Description	Example	Try it
 and 	Returns True if both statements are true	x < 5 and  x < 10	
