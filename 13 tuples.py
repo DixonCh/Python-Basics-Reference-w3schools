4898c67a49de63253dad7d0a55fc7e950de6d620
DixonCh <dixonchaudhary7@gmail.com>
2020-04-24 14:29:36 +0545

    Update 13 tuples.py

--- 13 tuples.py
+++ 13 tuples.py
@@ -1,5 +1,6 @@
 #Python Tuples
-'''Tuple
+'''
+Tuple
 A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
 
 Example
@@ -53,7 +54,7 @@ This example returns the items from index -4 (included) to index -1 (excluded)
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[-4:-1])
-
+'''
 Change Tuple Values
 Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
 
@@ -61,22 +62,24 @@ But there is a workaround. You can convert the tuple into a list, change the lis
 
 Example
 Convert the tuple into a list to be able to change it:
-
+'''
 x = ("apple", "banana", "cherry")
 y = list(x)
 y[1] = "kiwi"
 x = tuple(y)
 
 print(x)
+'''
 Loop Through a Tuple
 You can loop through the tuple items by using a for loop.
 
 Example
 Iterate through the items and print the values:
-
+'''
 thistuple = ("apple", "banana", "cherry")
 for x in thistuple:
   print(x)
+  '''
 You will learn more about for loops in our Python For Loops Chapter.
 
 Check if Item Exists
@@ -84,32 +87,40 @@ To determine if a specified item is present in a tuple use the in keyword:
 
 Example
 Check if "apple" is present in the tuple:
-
+'''
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
   print("Yes, 'apple' is in the fruits tuple")
+ 
+'''
 Tuple Length
 To determine how many items a tuple has, use the len() method:
 
 Example
 Print the number of items in the tuple:
+'''
 
 thistuple = ("apple", "banana", "cherry")
 print(len(thistuple))
+
+'''
 Add Items
 Once a tuple is created, you cannot add items to it. Tuples are unchangeable.
 
 Example
 You cannot add items to a tuple:
+'''
 
 thistuple = ("apple", "banana", "cherry")
 thistuple[3] = "orange" # This will raise an error
 print(thistuple)
+'''
 Create Tuple With One Item
 To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
 
 Example
 One item tuple, remember the commma:
+'''
 
 thistuple = ("apple",)
 print(type(thistuple))
@@ -117,6 +128,7 @@ print(type(thistuple))
 #NOT a tuple
 thistuple = ("apple")
 print(type(thistuple))
+'''
 Remove Items
 Note: You cannot remove items in a tuple.
 
@@ -124,29 +136,32 @@ Tuples are unchangeable, so you cannot remove items from it, but you can delete 
 
 Example
 The del keyword can delete the tuple completely:
-
+'''
 thistuple = ("apple", "banana", "cherry")
 del thistuple
 print(thistuple) #this will raise an error because the tuple no longer exists
+'''
 Join Two Tuples
 To join two or more tuples you can use the + operator:
 
 Example
 Join two tuples:
-
+'''
 tuple1 = ("a", "b" , "c")
 tuple2 = (1, 2, 3)
 
 tuple3 = tuple1 + tuple2
 print(tuple3)
+'''
 The tuple() Constructor
 It is also possible to use the tuple() constructor to make a tuple.
 
 Example
 Using the tuple() method to make a tuple:
-
+''' 
 thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
 print(thistuple)
+'''
 Tuple Methods
 Python has two built-in methods that you can use on tuples.
 
@@ -154,9 +169,11 @@ Method	Description
 count()	Returns the number of times a specified value occurs in a tuple
 index()	Searches the tuple for a specified value and returns the position of where it was found
 Test Yourself With Exercises
+'''
+'''
 Exercise:
 Print the first item in the fruits tuple.
-
+''' 
 fruits = ("apple", "banana", "cherry")
 print(
 )
