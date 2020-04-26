7950959cdf580eadc4a35c389c89f0f3e12d1fac
DixonCh <dixonchaudhary7@gmail.com>
2020-04-26 22:27:16 +0545

    Update 15 dictionaries.py

--- 15 dictionaries.py
+++ 15 dictionaries.py
@@ -38,10 +38,12 @@ Get the value of the "model" key:
 x = thisdict.get("model")
 
 '''
-Change Values
+Change Values:
+
 You can change the value of a specific item by referring to its key name:
 
-Example
+Example:
+
 Change the "year" to 2018:
 '''
 
@@ -52,7 +54,7 @@ thisdict = {
 }
 thisdict["year"] = 2018
 '''
-Loop Through a Dictionary
+Loop Through a Dictionary:
 You can loop through a dictionary by using a for loop.
 
 When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
