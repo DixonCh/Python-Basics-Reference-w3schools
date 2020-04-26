9366f9256cc32f9a17ba802a17e75e9fead632f5
DixonCh <dixonchaudhary7@gmail.com>
2020-04-26 22:18:14 +0545

    Update 15 dictionaries.py

--- 15 dictionaries.py
+++ 15 dictionaries.py
@@ -265,7 +265,7 @@ myfamily = {
 The dict() Constructor
 It is also possible to use the dict() constructor to make a new dictionary:
 
-Example
+Example:
 '''
 thisdict = dict(brand="Ford", model="Mustang", year=1964)
 # note that keywords are not string literals
