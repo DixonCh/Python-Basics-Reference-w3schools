9b51bb5c7420393b804aa281ca5f801bfd185a62
DixonCh <dixonchaudhary7@gmail.com>
2020-04-26 22:28:11 +0545

    Update 15 dictionaries.py

--- 15 dictionaries.py
+++ 15 dictionaries.py
@@ -55,11 +55,13 @@ thisdict = {
 thisdict["year"] = 2018
 '''
 Loop Through a Dictionary:
+
 You can loop through a dictionary by using a for loop.
 
 When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
 
-Example
+Example:
+
 Print all key names in the dictionary, one by one:
 '''
 
@@ -67,7 +69,7 @@ for x in thisdict:
   print(x)
   
 '''
-Example
+Example:
 Print all values in the dictionary, one by one:
 '''
 
