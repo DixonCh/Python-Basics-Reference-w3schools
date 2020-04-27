b52d2ac173febde42d97cec277a26b7e28727291
DixonCh <dixonchaudhary7@gmail.com>
2020-04-27 15:35:09 +0545

    Update 14 sets.py

--- 14 sets.py
+++ 14 sets.py
@@ -179,7 +179,8 @@ print(thisset)
 Set Methods
 Python has a set of built-in methods that you can use on sets.
 
-Method	Description
+Method	Description:
+  
 add()	Adds an element to the set
 clear()	Removes all the elements from the set
 copy()	Returns a copy of the set
