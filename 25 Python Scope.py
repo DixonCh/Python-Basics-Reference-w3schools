4d546412076f2dd18092addd694c6b4f3731c66b
DixonCh <dixonchaudhary7@gmail.com>
2020-04-29 13:56:30 +0545

    Update 25 Python Scope.py

--- 25 Python Scope.py
+++ 25 Python Scope.py
@@ -1,4 +1,4 @@
-Python Scope
+'''Python Scope
 A variable is only available from inside the region it is created. This is called scope.
 
 Local Scope
@@ -6,18 +6,19 @@ A variable created inside a function belongs to the local scope of that function
 
 Example
 A variable created inside a function is available inside that function:
-
+'''
 def myfunc():
   x = 300
   print(x)
 
 myfunc()
+'''
 Function Inside Function
 As explained in the example above, the variable x is not available outside the function, but it is available for any function inside the function:
 
 Example
 The local variable can be accessed from a function within the function:
-
+'''
 def myfunc():
   x = 300
   def myinnerfunc():
@@ -25,7 +26,7 @@ def myfunc():
   myinnerfunc()
 
 myfunc()
-
+'''
 Global Scope
 A variable created in the main body of the Python code is a global variable and belongs to the global scope.
 
@@ -33,7 +34,7 @@ Global variables are available from within any scope, global and local.
 
 Example
 A variable created outside of a function is global and can be used by anyone:
-
+'''
 x = 300
 
 def myfunc():
@@ -42,12 +43,13 @@ def myfunc():
 myfunc()
 
 print(x)
+'''
 Naming Variables
 If you operate with the same variable name inside and outside of a function, Python will treat them as two separate variables, one available in the global scope (outside the function) and one available in the local scope (inside the function):
 
 Example
 The function will print the local x, and then the code will print the global x:
-
+'''
 x = 300
 
 def myfunc():
@@ -57,6 +59,7 @@ def myfunc():
 myfunc()
 
 print(x)
+'''
 Global Keyword
 If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
 
@@ -64,7 +67,7 @@ The global keyword makes the variable global.
 
 Example
 If you use the global keyword, the variable belongs to the global scope:
-
+'''
 def myfunc():
   global x
   x = 300
@@ -72,10 +75,13 @@ def myfunc():
 myfunc()
 
 print(x)
+
+'''
 Also, use the global keyword if you want to make a change to a global variable inside a function.
 
 Example
 To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+'''
 
 x = 300
 
