4107e83210168303792504d8aa91a5444242396d
DixonCh <dixonchaudhary7@gmail.com>
2020-04-29 14:08:15 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -1,5 +1,7 @@
 '''Python For Loops
-Python For Loops
+
+Python For Loops:
+
 A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
 This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
