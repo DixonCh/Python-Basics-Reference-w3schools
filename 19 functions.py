2f827097ea5b607d91c1918e21ec01acfcfb8623
DixonCh <dixonchaudhary7@gmail.com>
2020-04-29 14:07:48 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -74,7 +74,8 @@ The range() Function:
 To loop through a set of code a specified number of times, we can use the range() function,
 The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
 
-Example
+Example:
+
 Using the range() function:
 '''
 for x in range(6):
