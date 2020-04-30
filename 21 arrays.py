831277805dd1b777ab8366a05f523348817ed267
DixonCh <dixonchaudhary7@gmail.com>
2020-04-30 20:48:38 +0545

    Update 21 arrays.py

--- 21 arrays.py
+++ 21 arrays.py
@@ -10,11 +10,12 @@ Example
 Create an array containing car names:
 '''
 cars = ["Ford", "Volvo", "BMW"]
+'''
 What is an Array?
 An array is a special variable, which can hold more than one value at a time.
 
 If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
-
+'''
 car1 = "Ford"
 car2 = "Volvo"
 car3 = "BMW"
