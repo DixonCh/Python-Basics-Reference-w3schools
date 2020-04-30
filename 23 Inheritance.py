4a94e5e8690b4be69d7f39c2c2ca545a680912f3
DixonCh <dixonchaudhary7@gmail.com>
2020-04-30 15:10:37 +0545

    Update 23 Inheritance.py

--- 23 Inheritance.py
+++ 23 Inheritance.py
@@ -1,5 +1,7 @@
-''' Python Inheritance
-Python Inheritance
+''' Python Inheritance:
+
+Python Inheritance:
+
 Inheritance allows us to define a class that inherits all the methods and properties from another class.
 
 Parent class is the class being inherited from, also called base class.
