930662fa428942910f379245f7c97bb9a274821f
DixonCh <dixonchaudhary7@gmail.com>
2020-06-05 19:53:30 +0545

    Update 08 casting.py

--- 08 casting.py
+++ 08 casting.py
@@ -1,5 +1,7 @@
-#Python Casting
-#Specify a Variable Type
+#Python Casting:
+
+#Specify a Variable Type:
+
 #There may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
 
 #Casting in python is therefore done using constructor functions:
