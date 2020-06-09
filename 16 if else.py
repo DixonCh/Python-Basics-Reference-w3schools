420af9dfe495032ea1bd345a08cf0e17c06e6b9f
DixonCh <dixonchaudhary7@gmail.com>
2020-06-09 15:07:12 +0545

    Update 16 if else.py

--- 16 if else.py
+++ 16 if else.py
@@ -14,7 +14,8 @@ These conditions can be used in several ways, most commonly in "if statements" a
 
 An "if statement" is written by using the if keyword.
 
-Example
+Example:
+
 If statement:
 '''
 a = 33
@@ -26,7 +27,7 @@ if b > a:
 In this example we use two variables, a and b, which are used as part of the if statement to test whether b is greater than a. 
 As a is 33, and b is 200, we know that 200 is greater than 33, and so we print to screen that "b is greater than a".
 
-Indentation
+Indentation:
 
 Python relies on indentation (whitespace at the beginning of a line) to define scope in the code. Other programming languages often use curly-brackets for this purpose.
 
