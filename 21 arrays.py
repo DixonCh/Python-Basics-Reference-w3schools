9b3bee9cac0da64de1c7e8fbef827d3219ab63fe
DixonCh <dixonchaudhary7@gmail.com>
2020-06-15 11:42:05 +0545

    updates

--- 21 arrays.py
+++ 21 arrays.py
@@ -1,7 +1,9 @@
-'''Python Arrays
+'''Python Arrays:
+
 Note: Python does not have built-in support for Arrays, but Python Lists can be used instead.
 
-Arrays
+Arrays:
+
 Note: This page shows you how to use LISTS as ARRAYS, however, to work with arrays in Python you will have to import a library, like the NumPy library.
 
 Arrays are used to store multiple values in one single variable:
