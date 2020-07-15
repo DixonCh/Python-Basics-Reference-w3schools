8ba09e8b58b8cad5542b41be3ea438989bce79e2
DixonCh <dixonchaudhary7@gmail.com>
2020-07-15 21:23:17 +0545

    Update 10 booleans.py

--- 10 booleans.py
+++ 10 booleans.py
@@ -1,7 +1,8 @@
 '''Python Booleans
 Booleans represent one of two values: True or False.
 
-Boolean Values
+Boolean Values:
+
 In programming you often need to know if an expression is True or False.
 
 You can evaluate any expression in Python, and get one of two answers, True or False.
