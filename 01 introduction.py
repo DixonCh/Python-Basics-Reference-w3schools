c8d30532a138fdacd59a3a4be9551dedcb4007f8
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-07-24 20:10:03 +0545

    Update 01 introduction.py

--- 01 introduction.py
+++ 01 introduction.py
@@ -1,4 +1,5 @@
 What is Python?
+
 Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
 
 It is used for:
@@ -40,6 +41,7 @@ Python Syntax compared to other programming languages.
 Python was designed for readability, and has some similarities to the English language with influence from mathematics.
 
 Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
+
 Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. 
 
 Other programming languages often use curly-brackets for this purpose.
