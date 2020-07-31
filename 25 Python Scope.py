9ff3763b3eae9be1bce272e125a8b45896bcd9ec
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-07-31 18:34:08 +0545

    Update 25 Python Scope.py

--- 25 Python Scope.py
+++ 25 Python Scope.py
@@ -1,10 +1,13 @@
-'''Python Scope
+'''Python Scope:
+
 A variable is only available from inside the region it is created. This is called scope.
 
-Local Scope
+Local Scope:
+
 A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
 
-Example
+Example:
+
 A variable created inside a function is available inside that function:
 '''
 def myfunc():
