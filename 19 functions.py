aa82ee1378a7daa3f0e12b30f9069b4a23cddcc8
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-08-18 06:37:16 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -88,7 +88,8 @@ Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
 
 The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6):
 
-Example
+Example:
+
 Using the start parameter:
 '''
 for x in range(2, 6):
@@ -97,7 +98,8 @@ for x in range(2, 6):
 '''
 The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
 
-Example
+Example:
+
 Increment the sequence with 3 (default is 1):
 '''
 for x in range(2, 30, 3):
