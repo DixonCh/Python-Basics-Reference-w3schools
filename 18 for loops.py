e19b5b1d6336f817a9f1979313ade440781f5e97
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-08-23 19:30:10 +0545

    Update 18 for loops.py

--- 18 for loops.py
+++ 18 for loops.py
@@ -78,7 +78,8 @@ To loop through a set of code a specified number of times, we can use the range(
 The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a 
 specified number.
 
-Example
+Example:
+
 Using the range() function:
 '''
 
@@ -110,10 +111,12 @@ for x in range(2, 30, 3):
   print(x)
  
 '''
-Else in For Loop
+Else in For Loop:
+
 The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
 
-Example
+Example:
+
 Print all numbers from 0 to 5, and print a message when the loop has ended:
 '''
 for x in range(6):
@@ -122,7 +125,8 @@ else:
   print("Finally finished!")
  
 '''
-Nested Loops
+Nested Loops:
+
 A nested loop is a loop inside a loop.
 
 The "inner loop" will be executed one time for each iteration of the "outer loop":
