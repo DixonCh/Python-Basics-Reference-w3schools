67ee7a926645d59756d0759824100d4d0ea882ca
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-09-25 07:04:54 +0545

    Update 22 Classes and Objects.py

--- 22 Classes and Objects.py
+++ 22 Classes and Objects.py
@@ -1,15 +1,19 @@
-Python Classes and Objects
-Python Classes/Objects
+Python Classes and Objects:
+  
+Python Classes/Objects:
+  
 Python is an object oriented programming language.
 
 Almost everything in Python is an object, with its properties and methods.
 
 A Class is like an object constructor, or a "blueprint" for creating objects.
 
-Create a Class
+Create a Class:
+  
 To create a class, use the keyword class:
 
-Example
+Example:
+  
 Create a class named MyClass, with a property named x:
 
 class MyClass:
