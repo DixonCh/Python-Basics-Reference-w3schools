1bf3717915934378a10edf691d4a28292d3b951d
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-12-09 07:47:55 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -106,10 +106,12 @@ for x in range(2, 30, 3):
   print(x)
   
 '''
-Else in For Loop
+Else in For Loop:
+
 The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
 
-Example
+Example:
+
 Print all numbers from 0 to 5, and print a message when the loop has ended:
 '''
 
