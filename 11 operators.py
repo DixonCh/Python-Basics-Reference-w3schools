a68578bd0c92c7c8486e65d37b1bd6636feecc91
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-12-29 21:15:02 +0545

    Update 11 operators.py

--- 11 operators.py
+++ 11 operators.py
@@ -64,7 +64,7 @@ Operator	Name	                Example	     Try it
 #Logical operators are used to combine conditional statements:
 
 '''
-Operator	Description	Example	Try it
+Operator	Description:
 
 and 	Returns True if both statements are true	x < 5 and  x < 10	
 or	Returns True if one of the statements is true	x < 5 or x < 4	
