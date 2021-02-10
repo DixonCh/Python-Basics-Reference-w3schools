d5d6b8fe3674ed0e44689b83efa76c9457e9796b
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-02-10 08:09:36 +0545

    Update 16 if else.py

--- 16 if else.py
+++ 16 if else.py
@@ -60,7 +60,8 @@ elif a == b:
 In this example a is equal to b, so the first condition is not true, but the elif condition is true, so we print to screen that
 "a and b are equal".
 
-Else
+Else:
+
 The else keyword catches anything which isn't caught by the preceding conditions.
 
 Example:
@@ -79,7 +80,8 @@ In this example a is greater than b, so the first condition is not true, also th
 
 You can also have an else without the elif:
 
-Example
+Example:
+
 '''
 a = 200
 b = 33
@@ -88,10 +90,12 @@ if b > a:
 else:
   print("b is not greater than a")
   '''
-Short Hand If
+Short Hand If:
+
 If you have only one statement to execute, you can put it on the same line as the if statement.
 
-Example
+Example:
+
 One line if statement:
 '''
 if a > b: print("a is greater than b")
@@ -99,7 +103,8 @@ if a > b: print("a is greater than b")
 Short Hand If ... Else
 If you have only one statement to execute, one for if, and one for else, you can put it all on the same line:
 
-Example
+Example:
+
 One line if else statement:
 '''
 a = 2
@@ -110,11 +115,13 @@ This technique is known as Ternary Operators, or Conditional Expressions.
 
 You can also have multiple else statements on the same line:
 
-Example
+Example:
+
 One line if else statement, with 3 conditions:
 '''
 a = 330
 b = 330
+
 print("A") if a > b else print("=") if a == b else print("B")
 '''
 And
