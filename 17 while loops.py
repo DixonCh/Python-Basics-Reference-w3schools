1d45d05739410476922ccd8ee2652170de754f97
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-02-14 08:36:33 +0545

    Update 17 while loops.py

--- 17 while loops.py
+++ 17 while loops.py
@@ -5,9 +5,11 @@ Python Loops:
 Python has two primitive loop commands:
 
 while loops
+
 for loops
 
 The while Loop:
+
 With the while loop we can execute a set of statements as long as a condition is true.
 
 Example:
