b730936758c2299e1266722808dc89c0cb96140a
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-02-28 07:55:39 +0545

    Update 20 lambda.py

--- 20 lambda.py
+++ 20 lambda.py
@@ -8,6 +8,7 @@ A lambda function can take any number of arguments, but can only have one expres
 Syntax:
 
 lambda arguments : expression
+
 The expression is executed and the result is returned:
 
 Example:
@@ -16,6 +17,7 @@ A lambda function that adds 10 to the number passed in as an argument, and print
 '''
 
 x = lambda a : a + 10
+
 print(x(5))
 '''
 Lambda functions can take any number of arguments:
@@ -32,15 +34,19 @@ Example:
 A lambda function that sums argument a, b, and c and print the result:
 '''
 x = lambda a, b, c : a + b + c
+
 print(x(5, 6, 2))
 
 Why Use Lambda Functions?
+
 The power of lambda is better shown when you use them as an anonymous function inside another function.
 
 Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number:
 
 def myfunc(n):
+  
   return lambda a : a * n
+
 Use that function definition to make a function that always doubles the number you send in:
 
 Example:
@@ -51,6 +57,7 @@ def myfunc(n):
 mydoubler = myfunc(2)
 
 print(mydoubler(11))
+
 Or, use the same function definition to make a function that always triples the number you send in:
 
 Example:
@@ -61,6 +68,7 @@ def myfunc(n):
 mytripler = myfunc(3)
 
 print(mytripler(11))
+
 Or, use the same function definition to make both functions, in the same program:
 
 Example:
