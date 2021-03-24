e437cca3c7b952922abf3318183ac85dc9ff3261
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-03-24 07:55:13 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -75,6 +75,7 @@ for x in fruits:
 The range() Function:
 
 To loop through a set of code a specified number of times, we can use the range() function,
+
 The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
 
 Example:
