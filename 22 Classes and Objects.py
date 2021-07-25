a9dc35cb5ae4dfcc634ecbf7ac3da719c7377c30
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-07-25 08:43:27 +0545

    Update 22 Classes and Objects.py

--- 22 Classes and Objects.py
+++ 22 Classes and Objects.py
@@ -77,14 +77,17 @@ class Person:
 
 p1 = Person("John", 36)
 p1.myfunc()
+
 Note: The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.
 
-The self Parameter
+The self Parameter:
+  
 The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
 
 It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
 
-Example
+Example:
+  
 Use the words mysillyobject and abc instead of self:
 
 class Person:
