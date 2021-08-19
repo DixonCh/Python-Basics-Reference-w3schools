177ac75d85fee37da87f5e0bc56d5373c2a5c6ea
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-08-19 18:12:04 +0545

    Update 11 operators.py

--- 11 operators.py
+++ 11 operators.py
@@ -66,14 +66,15 @@ Operator	Name	                Example	     Try it
 #Python Logical Operators:
 
 #Logical operators are used to combine conditional statements:
-
 '''
 Operator	Description:
 
 and 	Returns True if both statements are true	x < 5 and  x < 10	
 or	Returns True if one of the statements is true	x < 5 or x < 4	
 not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)	
-Python Identity Operators
+
+Python Identity Operators:
+
 Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
 
 Operator	Description	Example	Try it
