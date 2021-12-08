5da4ed017124a93a27526a14ade7813234b95dde
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-12-08 17:21:01 +0545

    Update 14 sets.py

--- 14 sets.py
+++ 14 sets.py
@@ -111,6 +111,7 @@ print(thisset)
 Note: If the item to remove does not exist, discard() will NOT raise an error.
 
 You can also use the pop(), method to remove an item, but this method will remove the last item. Remember that sets are unordered, 
+
 so you will not know what item that gets removed.
 
 The return value of the pop() method is the removed item.
