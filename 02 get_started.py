f4bfaff3a6d6eee1dd955184ae6f9c21d0050ffc
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-12-12 09:36:47 +0545

    Update 02 get_started.py

--- 02 get_started.py
+++ 02 get_started.py
@@ -4,6 +4,7 @@ Python Install:
 Many PCs and Macs will have python already installed.
 
 To check if you have python installed on a Windows PC, 
+
 search in the start bar for Python or run the following on the Command Line (cmd.exe):
 
 C:\Users\Your Name>python --version
