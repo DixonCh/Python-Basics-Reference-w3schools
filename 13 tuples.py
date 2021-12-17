5a5d1c720eaa901d59b4aa578a4c2e2afff8d743
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-12-17 09:17:15 +0545

    Update 13 tuples.py

--- 13 tuples.py
+++ 13 tuples.py
@@ -117,7 +117,9 @@ Example:
 Check if "apple" is present in the tuple:
 '''
 thistuple = ("apple", "banana", "cherry")
+
 if "apple" in thistuple:
+  
   print("Yes, 'apple' is in the fruits tuple")
  
 '''
