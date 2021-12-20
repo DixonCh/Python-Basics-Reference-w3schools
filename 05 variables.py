cf4d7c2b8dbfaa5b173a5fb6462b75a3b86d8c34
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-12-20 13:45:52 +0545

    Update 05 variables.py

--- 05 variables.py
+++ 05 variables.py
@@ -139,7 +139,8 @@ def myfunc():
 myfunc()
 If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
 
-Example
+Example:
+  
 Create a variable inside a function, with the same name as the global variable
 
 x = "awesome"
