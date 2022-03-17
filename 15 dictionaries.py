0515e0f457ff9ce892e60b21803c57f7b0fa81bf
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-03-17 11:25:45 +0545

    Update 15 dictionaries.py

--- 15 dictionaries.py
+++ 15 dictionaries.py
@@ -3,6 +3,7 @@
 Dictionary:
 
 A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and 
+
 they have keys and values.
 
 Example:
