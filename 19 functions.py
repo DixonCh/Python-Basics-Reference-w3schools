4828f00877afde663ec8f0c1562d944e5560c851
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-03-27 10:27:54 +0545

    Update 19 functions.py

--- 19 functions.py
+++ 19 functions.py
@@ -11,11 +11,11 @@ With the for loop we can execute a set of statements, once for each item in a li
 Example:
 
 Print each fruit in a fruit list:
+
 '''
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
- 
 '''
 The for loop does not require an indexing variable to set beforehand.
 
