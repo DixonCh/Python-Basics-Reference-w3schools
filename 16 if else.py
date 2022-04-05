2db4924b15a6e47d24c88b19d26808be0bd6d6f3
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-04-05 09:27:55 +0545

    Update 16 if else.py

--- 16 if else.py
+++ 16 if else.py
@@ -59,6 +59,7 @@ elif a == b:
   print("a and b are equal")
   
 '''
+
 In this example a is equal to b, so the first condition is not true, but the elif condition is true, so we print to screen that
 "a and b are equal".
 
