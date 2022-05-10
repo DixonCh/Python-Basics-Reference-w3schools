39c9015b2f0a64d9b26dc9bb14c482acc8349749
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-05-10 10:51:20 +0545

    Update 17 while loops.py

--- 17 while loops.py
+++ 17 while loops.py
@@ -41,7 +41,6 @@ while i < 6:
   i += 1
   
 '''
-
 The continue Statement:
 
 With the continue statement we can stop the current iteration, and continue with the next:
@@ -56,7 +55,6 @@ while i < 6:
   if i == 3:
     continue
   print(i)
- 
 '''
 
 The else Statement:
