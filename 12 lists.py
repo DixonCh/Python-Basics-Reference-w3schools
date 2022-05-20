e2a225327b8851ee557125b28f508e0d844e9be2
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-05-20 07:29:42 +0545

    Update 12 lists.py

--- 12 lists.py
+++ 12 lists.py
@@ -45,7 +45,6 @@ Negative Indexing:
 Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last item etc.
 
 Example:
-  
 Print the last item of the list:
 
 thislist = ["apple", "banana", "cherry"]
