2cc96903fd6350c662cbfac6ab9057f28732dcda
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-07-02 08:14:40 +0545

    Update 13 tuples.py

--- 13 tuples.py
+++ 13 tuples.py
@@ -27,6 +27,7 @@ thistuple = ("apple", "banana", "cherry")
 
 print(thistuple[1])
 '''
+
 Negative Indexing:
 
 Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last item etc.
