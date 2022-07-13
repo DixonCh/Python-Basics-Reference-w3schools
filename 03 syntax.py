f31be46a1641b5ede3b1da8838b9cbb09185f35d
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-07-13 08:02:30 +0545

    Update 03 syntax.py

--- 03 syntax.py
+++ 03 syntax.py
@@ -15,11 +15,13 @@ As we learned in the previous page, Python syntax can be executed by writing dir
 '''
 
 >>> print("Hello, World!")
+
 Hello, World!
 Or by creating a python file on the server, using the .py file extension, and running it in the Command Line:
 
   '''
 C:\Users\Your Name>python myfile.py
+
 Python Indentation:
 
 Indentation refers to the spaces at the beginning of a code line.
